7f905b56b1423ace0591ae5ee24ce235dcec5639
Henrique Breim <henrique@breim.com.br>
2017-06-11 03:06:04 -0300

    finish download

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,15 @@ class PagesController < ApplicationController
   end
 
   def download
-  	mechanize = Mechanize.new
-	page = mechanize.get("http://www.youtubeinmp3.com/download/?video=#{params[:music]}")
-	link = page.at('.download-buttons a/@href').value
-	download_url = 'http://www.youtubeinmp3.com/' + link
-	redirect_to download_url
+    begin
+      mechanize = Mechanize.new
+      page = mechanize.get("http://www.youtubeinmp3.com/download/?video=#{params[:music]}")
+      link = page.at('.download-buttons a/@href').value
+      download_url = 'http://www.youtubeinmp3.com/' + link
+      redirect_to download_url
+    rescue Exception
+      flash[:error] = 'Check your link'
+      redirect_to root_path
+    end
   end
 end
