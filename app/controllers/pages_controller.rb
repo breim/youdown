4bce39d6e92941756ffd5e666d4fae8b3d50e6c0
Henrique Breim <henrique@breim.com.br>
2017-06-19 19:58:13 -0300

    test button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
       mechanize = Mechanize.new
       page = mechanize.get("http://www.youtubeinmp3.com/download/?video=#{params[:music]}")
       link = page.at('.download-buttons a/@href').value
-      download_url = 'http://www.youtubeinmp3.com' + link
+      download_url = 'http://www.youtubeinmp3.com' + link + '&progressType=button'
       redirect_to download_url
     rescue Exception
       flash[:error] = 'Check your link'
